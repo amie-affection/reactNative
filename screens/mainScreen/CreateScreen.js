2372f8a2446b65312ab048239d33b22986401bbc
amie-affection <amie.affection@gmail.com>
2020-08-13 23:26:58 +0300

    added Location

--- screens/mainScreen/CreateScreen.js
+++ screens/mainScreen/CreateScreen.js
@@ -1,26 +1,50 @@
 import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
-import { Camera } from "expo-camera";
 import { TouchableOpacity } from "react-native-gesture-handler";
 
-const CreateScreen = () => {
+import { Camera } from "expo-camera";
+import * as Location from "expo-location";
+
+const CreateScreen = ({ navigation }) => {
   const [camera, setCamera] = useState(null);
-    const [photo, setPhoto] = useState(null);
+  const [photo, setPhoto] = useState("");
 
   const takePhoto = async () => {
     console.log(camera);
-    console.log("camera", camera.takePictureAsync());
-    const photo = await camera.takePictureAsync();
-    console.log(photo.uri);
+    // console.log("camera", camera.takePictureAsync());
+
+    // const photo = await camera.takePictureAsync();
+    // console.log(photo.uri);
+    const location = await Location.getCurrentPositionAsync();
+    // setPhoto(photo.uri);
+    // console.log(photo);
+  };
+
+  const sendPhoto = () => {
+    console.log("navigation", navigation);
+    navigation.navigate("Posts", { photo });
   };
 
   return (
     <View style={styles.container}>
       <Camera style={styles.camera} ref={setCamera}>
+        {/* {photo && (
+          <View style={styles.takePhotoContainer}>
+            <Image
+              source={{ uri: photo }}
+              style={{ height: 200, width: 200 }}
+            />
+          </View>
+        )} */}
         <TouchableOpacity onPress={takePhoto} style={styles.snapContainer}>
           <Text style={styles.snap}>SNAP</Text>
         </TouchableOpacity>
       </Camera>
+      <View>
+        <TouchableOpacity onPress={sendPhoto} style={styles.buttonContainer}>
+          <Text style={styles.sendTitle}>SEND</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -31,6 +55,9 @@ const styles = StyleSheet.create({
   },
   camera: {
     flex: 1,
+    marginHorizontal: 2,
+    marginTop: 20,
+    borderRadius: 10,
     alignItems: "center",
     justifyContent: "flex-end",
   },
@@ -47,6 +74,30 @@ const styles = StyleSheet.create({
     height: 70,
     justifyContent: "center",
     alignItems: "center",
+  },
+  takePhotoContainer: {
+    position: "absolute",
+    top: 50,
+    left: 10,
+    borderColor: "#fff",
+    borderWidth: 1,
+    height: 200,
+    width: 200,
+  },
+  buttonContainer: {
+    marginHorizontal: 30,
+    height: 40,
+    borderWidth: 2,
+    borderColor: "#20b2aa",
+    borderRadius: 10,
+    marginTop: 20,
+    marginBottom: 10,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  sendTitle: {
+    color: "#20b2aa",
+    fontSize: 20,
   },
 });
 
--- screens/mainScreen/PostsScreen.js
+++ screens/mainScreen/PostsScreen.js
@@ -1,10 +1,40 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, StyleSheet, FlatList } from "react-native";
 
-const PostsScreen = () => {
+const PostsScreen = ({ route }) => {
+  const [posts, setPosts] = useState([]);
+  console.log("route.params", route.params);
+
+  useEffect(() => {
+    if (route.params) {
+      setPosts((prevState) => [...prevState, route.params]);
+    }
+  }, [route.params]);
+  console.log("posts", posts);
+
   return (
     <View style={styles.container}>
-      <Text>PostsScreen</Text>
+      <FlatList
+        data={posts}
+        keyExtractor={(item, indx) => indx.toString()}
+        renderItem={({ item }) => (
+          <View
+            style={{
+              marginBottom: 10,
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Image
+              source={{ uri: item.photo }}
+              style={{
+                width: 350,
+                height: 200,
+              }}
+            />
+          </View>
+        )}
+      />
     </View>
   );
 };
